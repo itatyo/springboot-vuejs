9aa7c2e4aeb68a8339e70795196adf913430a16d
itatyo <h.inotty@gmail.com>
2021-04-30 20:05:47 +0900

    動作確認用で書いた処理の削除 設定ファイルの yml 化

--- src/main/java/dev/itatyo/demo/DemoApplication.java
+++ src/main/java/dev/itatyo/demo/DemoApplication.java
@@ -7,17 +7,7 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-    }
-
-    private ClerkMapper clerkmapper;
-    public DemoApplication (ClerkMapper clerkMapper) {
-        this.clerkmapper = clerkMapper;
-    }
-    @Bean
-    CommandLineRunner sampleCommandLineRunner() {
-        return args -> System.out.println(this.clerkmapper.findByID(1));
     }
 }
