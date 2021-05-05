6e4def623b8647ec60ff9889f844aadcafe9e490
itatyo <h.inotty@gmail.com>
2021-05-05 18:32:40 +0900

    id をパスで受けられるようにした

--- src/main/java/dev/itatyo/demo/restservice/controller/ClerkController.java
+++ src/main/java/dev/itatyo/demo/restservice/controller/ClerkController.java
@@ -2,10 +2,7 @@ package dev.itatyo.demo.restservice.controller;
 
 import dev.itatyo.demo.Clerk;
 import dev.itatyo.demo.ClerkMapper;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
@@ -18,6 +15,14 @@ public class ClerkController {
 
     @GetMapping("/clerk")
     public Clerk clerk(@RequestParam(value = "id", defaultValue = "1") int id) {
+        return getClerk(id);
+    }
+    @GetMapping("/clerk/{id}")
+    public Clerk clerk_id (@PathVariable("id") int id) {
+        return getClerk(id);
+    }
+
+    private Clerk getClerk(int id) {
         Clerk clerk = clerkMapper.findByID(id);
         if (clerk.equals(null)) {
             throw new RuntimeException();
